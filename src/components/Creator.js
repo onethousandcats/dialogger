bebd151786d37500e7e3a2645d7be242c9bd328d
hashirama senju <phillip.bercot@gmail.com>
2024-11-08 16:20:02 -0500

    adding fader to entries

--- src/components/Creator.js
+++ src/components/Creator.js
@@ -1,25 +1,58 @@
 import { Pencil } from "lucide-react";
 import Row from "./Row";
 import { useState } from "react";
+import { useStore } from "@nanostores/react";
+import { $entries } from "../stores/entries";
+import { $name, defaultName } from "../stores/name";
 
 export default () => {
-    const [name, setName] = useState('new_dialogue');
+    const entries = useStore($entries);
+    const name = useStore($name);
 
-    const [index, setindex] = useState(0);
+    const placeholderText = 'start dialogue entry';
+
+    const [entry, setEntry] = useState(placeholderText);
+    const [index, setIndex] = useState(0);
 
     const handleNameChange = (e) => {
-        setName(e.target.value);
+        $name.set(e.target.value === '' ? defaultName : e.target.value);
+    }
+
+    const handleEntryChange = (e) => {
+        setEntry(e.target.value);
+    }
+
+    const handleFocus = (e, text, setter) => {
+        if (e.target.value === text) {
+            setter('');
+        }
+    }
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            $entries.setKey(index, e.target.value);
+            setEntry('');
+            setIndex(index + 1);
+        }
     }
 
     return (
-        <div>
-            <Row><input value={name} onChange={handleNameChange} /><Pencil /></Row>
-            <div className="dialogger">
-                <Row>
-                    <label>{ index + 1 }</label>
-                    <input className="line" value={'start dialogue entry'} />
-                </Row>
+        <>
+            <Row><input value={name} onFocus={(e) => handleFocus(e, defaultName, $name.set)} onChange={handleNameChange} /><Pencil /></Row>
+            <div className="fader">
+                <div className="dialogger">
+                    { Object.entries(entries).map(([key, value]) => (
+                        <Row>
+                            <label>{ parseInt(key) + 1 }</label>
+                            <label>{ value }</label>
+                        </Row>
+                    ))}
+                    <Row className="test">
+                        <label>{ index + 1 }</label>
+                        <input onKeyDown={handleKeyDown} onFocus={(e) => handleFocus(e, placeholderText, setEntry)} onChange={handleEntryChange} className="line" value={entry} />
+                    </Row>
+                </div>        
             </div>
-        </div>
+        </>
     );
 }
---
+++ src/stores/name.js
@@ -0,0 +1,4 @@
+import { atom } from "nanostores";
+
+export const defaultName = 'new_dialogue';
+export const $name = atom(defaultName);
